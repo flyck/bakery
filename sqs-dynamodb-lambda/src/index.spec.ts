d43bc4d21e7e9cecc83365ea06c6e19bfd7b08d3
flyck <felix.brilej@googlemail.com>
2023-09-10 09:16:43 +0200

    feat(sqs-dynamodb-lambda): save more items

--- sqs-dynamodb-lambda/src/index.spec.ts
+++ sqs-dynamodb-lambda/src/index.spec.ts
@@ -6,7 +6,7 @@ const sqsTestEvent = {
     {
       "messageId": "19dd0b57-b21e-4ac1-bd88-01bbb068cb78",
       "receiptHandle": "MessageReceiptHandle",
-      "body": "Hello from SQS!",
+      "body": JSON.stringify([{ name: "alph" }, { name: "bims" }, { name: "bongo" }]),
       "attributes": {
         "ApproximateReceiveCount": "1",
         "SentTimestamp": "1523232000000",
@@ -18,7 +18,23 @@ const sqsTestEvent = {
       "eventSource": "aws:sqs",
       "eventSourceARN": "arn:aws:sqs:us-east-1:123456789012:MyQueue",
       "awsRegion": "us-east-1"
-    }
+    },
+    {
+      "messageId": "19dd0b57-b21e-4ac1-bd88-01bbb068cb78",
+      "receiptHandle": "MessageReceiptHandle",
+      "body": JSON.stringify([{ name: "doggo" }]),
+      "attributes": {
+        "ApproximateReceiveCount": "1",
+        "SentTimestamp": "1523232000000",
+        "SenderId": "123456789012",
+        "ApproximateFirstReceiveTimestamp": "1523232000001"
+      },
+      "messageAttributes": {},
+      "md5OfBody": "{{{md5_of_body}}}",
+      "eventSource": "aws:sqs",
+      "eventSourceARN": "arn:aws:sqs:us-east-1:123456789012:MyQueue",
+      "awsRegion": "us-east-1"
+    },
   ]
 }
 
--- sqs-dynamodb-lambda/src/index.ts
+++ sqs-dynamodb-lambda/src/index.ts
@@ -1,6 +1,7 @@
 import { DynamoDB } from "@aws-sdk/client-dynamodb"
 import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb"
 import { SQSEvent } from "aws-lambda"
+import { z } from "zod";
 // doesnt build 👇 https://github.com/oven-sh/bun/issues?q=is%3Aissue+is%3Aopen+child_process
 // import { fromNodeProviderChain } from "@aws-sdk/credential-providers"
 
@@ -15,30 +16,67 @@ import { defaultProvider } from "@aws-sdk/credential-provider-node"
 
 const table = process.env["TABLE_NAME"]
 
+const messageItem = z.object({
+  id: z.optional(z.number()),
+  name: z.string(),
+}).array();
+
+type DynamoDbItem = z.infer<typeof dynamoDbItem>
+const dynamoDbItem = z.object({
+  id: z.number(),
+  name: z.string(),
+});
+
+
 async function handler(event: SQSEvent) {
-  console.info("Received Event: ", JSON.stringify(event))
+  console.info("Received Events: ", JSON.stringify(event))
 
-  const client = new DynamoDB({
-    region: "eu-central-1",
-    credentials: defaultProvider()
+  let items: DynamoDbItem[] = []
+  event.Records.forEach((record) => {
+    const message = messageItem.parse(JSON.parse(record.body))
+
+    message.forEach((item) => {
+      if (!item.id) {
+        item.id = Math.floor(Math.random() * 100000) + 1
+      }
+
+      items.push({
+        id: item.id,
+        name: item.name
+      })
+    })
   })
-  const docClient = DynamoDBDocumentClient.from(client);
-  const putCommand = new PutCommand({
-    TableName: table,
-    Item: {
-      id: 123,
-      CommonName: "Shiba Inu",
-    },
-  });
 
-  console.info("Adding item to table...")
-  await docClient.send(putCommand)
-  console.info("Operation complete");
+  await save(items)
 
   return {
     status: 200,
     message: "success"
   }
+}
+
+async function save(items: DynamoDbItem[]) {
+  console.log("Saving items")
+
+  const client = new DynamoDB({
+    region: "eu-central-1",
+    credentials: defaultProvider()
+  })
+  const docClient = DynamoDBDocumentClient.from(client)
+
+
+  console.info("Adding items to table...")
+
+  await Promise.all(items.map(async (item) => {
+    const putCommand = new PutCommand({
+      TableName: table,
+      Item: item,
+    })
+    await docClient.send(putCommand)
+    console.log("Saved: ", JSON.stringify(item))
+  }))
+
+  console.info("Operation complete")
 }
 
 export { handler }
